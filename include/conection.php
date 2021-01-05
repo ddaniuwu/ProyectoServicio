d02e2a6bba0dcbc88d8d1b620286a96c4e3b2c28
ddaniuwu <luis.daniel132@outlook.es>
2021-01-05 17:18:55 -0600

    Carpeta con archivos back

---
+++ include/conection.php
@@ -0,0 +1,34 @@
+<?php
+$email=$_POST['mail'];
+$password=$_POST['password'];
+$mysqli= new mysqli("localhost","root","","BDtesci");
+if($mysqli===false){
+	die("ERROR:NO SE ESTABLECIO LA CONEXION".mysqli_connect_error());
+}
+
+$sql="SELECT NombrePrincipal,NoControlPrincipal FROM tblControlUsuarios WHERE NombrePrincipal = '$email' AND NoControlPrincipal  = '$password'";
+if($result=$mysqli->query($sql)){
+	if($result->num_rows>0){
+		while($row=$result->fetch_array()){
+			echo '<script type="text/javascript">
+        alert("Bienvenido");
+        window.location.href="../templates/home.html";
+        </script> ';
+          session_start();
+        		$_SESSION["_usuario"] = $usu;
+
+		}
+		$result->close();
+	}else{
+		echo '<script type="text/javascript">
+        alert("Nombre o contraseña incorrectos");
+        window.location.href="../index.html";
+        </script> ';
+	}
+}else{
+	echo "ERROR:No fue posible ejecutar $sql".$mysqli->error;
+}
+
+}
+$mysqli->close();
+?>
